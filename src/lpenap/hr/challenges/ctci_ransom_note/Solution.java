4b5d4f6b4cc8e29089fc031a7b7144fd3007b5c8
Luis Augusto Peña Pereira <lpenap@gmail.com>
2017-08-07 12:23:57 -0400

    Add approved solution for ctci Ransom Note problem

--- src/lpenap/hr/challenges/ctci_ransom_note/Solution.java
+++ src/lpenap/hr/challenges/ctci_ransom_note/Solution.java
@@ -2,13 +2,17 @@ package lpenap.hr.challenges.ctci_ransom_note;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * Solution order:  Problem statement:
+ * Solution order: O(n) Problem statement:
  * https://www.hackerrank.com/challenges/ctci-ransom-note .
  * 
- * Solution approach: 
+ * Solution approach: Create a hash to keep count of magazine
+ * words. Then substract each word found on the hash form the
+ * ransom word. If all the words from the ransom note can
+ * be decremented from the hash, then the answer is Yes.
  * 
  * @author lpenap
  *
@@ -20,13 +24,35 @@ public class Solution {
 		Scanner in = new Scanner(br);
 		int m = in.nextInt();
 		int n = in.nextInt();
-		String magazine[] = new String[m];
+		HashMap<String, Integer> magazine = new HashMap<String, Integer>();
 		for (int magazine_i = 0; magazine_i < m; magazine_i++) {
-			magazine[magazine_i] = in.next();
+			String word = in.next();
+			int count = 1;
+			if (magazine.containsKey(word)) {
+				count += magazine.get(word);
+			}
+			magazine.put(word, count);
 		}
-		String ransom[] = new String[n];
+		
+		boolean canWriteRansom = true;
 		for (int ransom_i = 0; ransom_i < n; ransom_i++) {
-			ransom[ransom_i] = in.next();
+			String word = in.next();
+			if (magazine.containsKey(word)) {
+				int currentCount = magazine.get(word) - 1;
+				if (currentCount > 0) {
+					magazine.put(word, currentCount);
+				} else {
+					magazine.remove(word);
+				}
+			} else {
+				canWriteRansom = false;
+				break;
+			}
+		}
+		if (canWriteRansom) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
 		}
 	}
 
